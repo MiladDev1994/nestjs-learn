bb4c3a2f3b9bb41c178188c7e62ff4393c90c0b7
milad goli <miladgolinezhad73@gmail.com>
2024-08-26 21:36:12 +0330

    multer

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, HttpStatus, ParseFilePipeBuilder, Post, Req, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
 import { JwtAuthGuard } from './auth/jwt-guard';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
 
 @Controller()
 export class AppController {
@@ -19,6 +21,63 @@ export class AppController {
     request,
   ): string {
     return request.user
+  }
+
+
+  @Post("upload")
+  @UseInterceptors(FileInterceptor("file", {
+    storage: diskStorage({
+      destination: "./upload/files",
+      filename: (req, file, cb) => {
+        cb(null, file.originalname)
+      }
+    }),
+    limits: {
+      fileSize: 10000000
+    }
+  }))
+  uploadFile(
+    @UploadedFile()
+    file: Express.Multer.File
+  ) {
+    console.log(file);
+    return {
+      data: file,
+      message: "Uploaded"
+    }
+  }
+
+
+
+  @Post("upload_mp3")
+  @UseInterceptors(FileInterceptor("file", {
+    storage: diskStorage({
+      destination: "./upload/files",
+      filename: (req, file, cb) => {
+        cb(null, file.originalname)
+      }
+    }),
+    limits: {
+      fileSize: 10000000
+    }
+  }))
+  uploadMp3File(
+    @UploadedFile(
+      new ParseFilePipeBuilder()
+        .addFileTypeValidator({
+          fileType: "mpeg"
+        })
+        .build({
+          errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY
+        })
+    )
+    file: Express.Multer.File
+  ) {
+    console.log(file);
+    return {
+      data: file,
+      message: "Uploaded"
+    }
   }
 }
 
